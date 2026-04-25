94f1768998bc69b126889594afd6f90c76ee4b05
souiiii <b423037@iiit-bh.ac.in>
2026-04-25 21:00:10 +0530

    feat: add HytaleServersPage component with HytaleHero integration

---
+++ src/pages/HytaleServersPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import HytaleHero from '../components/HytaleHero/HytaleHero';
+
+const HytaleServersPage = () => {
+  return (
+    <>
+      <HytaleHero />
+    </>
+  );
+};
+
+export default HytaleServersPage;
