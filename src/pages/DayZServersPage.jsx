f13f0599f55b839decf3e90624e9b9da2f6a268b
souiiii <b423037@iiit-bh.ac.in>
2026-04-25 20:07:55 +0530

    feat: add DayZServersPage component with hero, features, tiers, and workloads sections

--- src/pages/DayZServersPage.jsx
+++ src/pages/DayZServersPage.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import DayZHero from '../components/DayZHero/DayZHero';
 import DayZFeatures from '../components/DayZFeatures/DayZFeatures';
+import DayZInstanceTiers from '../components/DayZInstanceTiers/DayZInstanceTiers';
+import DayZWorkloads from '../components/DayZWorkloads/DayZWorkloads';
 
 const DayZServersPage = () => {
   return (
     <>
       <DayZHero />
       <DayZFeatures />
+      <DayZInstanceTiers />
+      <DayZWorkloads />
     </>
   );
 };
