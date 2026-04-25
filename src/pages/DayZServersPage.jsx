4aaa954b0a9fb0f3f4f39c199ffda645ed10e9a4
souiiii <b423037@iiit-bh.ac.in>
2026-04-25 20:02:25 +0530

    feat: add DayZServersPage and corresponding DayZFeatures component styles

--- src/pages/DayZServersPage.jsx
+++ src/pages/DayZServersPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import DayZHero from '../components/DayZHero/DayZHero';
+import DayZFeatures from '../components/DayZFeatures/DayZFeatures';
 
 const DayZServersPage = () => {
   return (
     <>
       <DayZHero />
+      <DayZFeatures />
     </>
   );
 };
