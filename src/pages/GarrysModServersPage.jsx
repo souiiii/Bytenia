06660505d7366da27d05693e4d81f7be976e1286
souiiii <b423037@iiit-bh.ac.in>
2026-04-25 19:49:15 +0530

    feat: add GarrysModServersPage and associated FAQ section component styles

--- src/pages/GarrysModServersPage.jsx
+++ src/pages/GarrysModServersPage.jsx
@@ -3,6 +3,7 @@ import GarrysModHero from '../components/GarrysModHero/GarrysModHero';
 import GarrysModDeployment from '../components/GarrysModDeployment/GarrysModDeployment';
 import GarrysModInstanceTiers from '../components/GarrysModInstanceTiers/GarrysModInstanceTiers';
 import GarrysModProtection from '../components/GarrysModProtection/GarrysModProtection';
+import GarrysModFAQ from '../components/GarrysModFAQ/GarrysModFAQ';
 
 const GarrysModServersPage = () => {
   return (
@@ -11,6 +12,7 @@ const GarrysModServersPage = () => {
       <GarrysModDeployment />
       <GarrysModInstanceTiers />
       <GarrysModProtection />
+      <GarrysModFAQ />
     </>
   );
 };
