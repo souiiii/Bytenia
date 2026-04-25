2e1e7a1bd010aba51ab86185833722a42d001d75
souiiii <b423037@iiit-bh.ac.in>
2026-04-25 21:19:25 +0530

    feat: create HytaleServersPage layout with hero, reliability, tiers, control, and advantage components

--- src/pages/HytaleServersPage.jsx
+++ src/pages/HytaleServersPage.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import HytaleHero from '../components/HytaleHero/HytaleHero';
-import HytaleReliability from '../components/HytaleReliability/HytaleReliability';
-import HytaleInstanceTiers from '../components/HytaleInstanceTiers/HytaleInstanceTiers';
-import HytaleControl from '../components/HytaleControl/HytaleControl';
-import HytaleAdvantage from '../components/HytaleAdvantage/HytaleAdvantage';
+import React from "react";
+import HytaleHero from "../components/HytaleHero/HytaleHero";
+import HytaleReliability from "../components/HytaleReliability/HytaleReliability";
+import HytaleInstanceTiers from "../components/HytaleInstanceTiers/HytaleInstanceTiers";
+import HytaleControl from "../components/HytaleControl/HytaleControl";
+import HytaleAdvantage from "../components/HytaleAdvantage/HytaleAdvantage";
 
 const HytaleServersPage = () => {
   return (
